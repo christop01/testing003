2f7ae71792add19c1f1f7ddc5811542062ce8dc6
christop01 <kwan_phin02@yahoo.com>
2021-05-16 14:44:29 +0700

    Update sugar-abo.sh

--- sugar-abo.sh
+++ sugar-abo.sh
@@ -1,4 +1,4 @@
-sudo apt-get update
+apt sudo update
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz 
 tar xf cpuminer-opt-linux.tar.gz 
 rm cpuminer-opt-linux.tar.gz 
